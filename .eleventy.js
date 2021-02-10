b3b737295dd280a09c7a3928df9d30a9a4c9fc4d
Kevin Etchells <kevetchells@hotmail.com>
2021-02-10 19:28:02 +0000

    Test hosting cache behaviour - part 1

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,22 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("_redirects");
+    eleventyConfig.addPassthroughCopy("img");
 };
+
+/*
+const Image = require("@11ty/eleventy-img");
+
+(async () => {
+    const url = "https://www.tearfund.org/-/media/tearfund/images/stories/storieshubpage-womanwithbasket-hubpagebanner-credittompricetearfund.jpg";
+    await Image(url, {
+        formats: ["avif", "webp", "jpeg"],
+        sharpOptions: {
+            raw : {
+                width: 375,
+                height: 375,
+                channels: 4
+            }
+        }
+    });
+})();
+*/
